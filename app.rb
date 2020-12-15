4776dc323363a4ebb4ed42a41a27cac0f375eb8d
github username <github email address>
2020-12-16 00:38:13 +0200

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
+    get "/" do 
+        erb :user_input
+    end
+
+    post "/" do 
+        @pig_latin = Piglatinize.new.piglatinize(params[:user_phrase].to_s)
+        erb :piglatinize
+    end 
+
+    post "/piglatinize" do 
+        @user_phrase 
+        # erb :piglatinize
+    end 
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,17 @@
+class PigLatinizer
+    def piglatinize(user_phrase)
+        pig_array = []
+        user_phrase_array = user_phrase.split(" ")
+        user_phrase_array.each do |word|
+        first_vowel = word.scan(/[aeiouAEIOU]/).first
+        word_array = word.split(first_vowel, 2)
+        if word_array[0] == ""
+            pig_latin_word = [first_vowel, word_array[1], "way"].join
+        else  
+            pig_latin_word = [first_vowel, word_array[1], word_array[0], "ay"].join
+        end
+        pig_array << pig_latin_word
+    end
+    pig_array.join(" ")
+   end
+end 
